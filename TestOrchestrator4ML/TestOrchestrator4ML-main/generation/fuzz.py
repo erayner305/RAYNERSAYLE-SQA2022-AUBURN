a2c6cdb41eb0cf183fdf54f3f0e24abb5b4b797e
erayner305 <42623568+erayner305@users.noreply.github.com>
2022-12-01 23:43:39 -0600

    Delete fuzz.py

--- TestOrchestrator4ML/TestOrchestrator4ML-main/generation/fuzz.py
+++
@@ -1,7 +0,0 @@
-import detection.main
-if __name__ == '__main__':
-	# Input invalid parms to generateUnitTest
-	detection.main.get_test_details('not a test at all') #passing something that is not a test
-	generateUnitTest('invalidAlgo', 'random') #passing an invalid filename for the algorithm
-	generateAttack(12, delta) #passing the incorrect data type
-	runs([1]) #passing an invalid list to the method
